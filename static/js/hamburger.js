787a8875469fec66d8b86f905c2998a58efe81da
Stuart Marsh <57923872+stujmar@users.noreply.github.com>
2021-03-20 18:03:42 -0500

    [sm] got menu animating

--- static/js/hamburger.js
+++ static/js/hamburger.js
@@ -4,12 +4,17 @@ for (let i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
     let content = document.getElementById("menu-toggle");
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
+    // if (content.style.display === "block") {
+    //   content.style.display = "none";
+    // } else {
+    //   content.style.display = "block";
+    // }
+    if (content.style.maxHeight == "80px") {
+        content.style.maxHeight = 0;
+      } else {
+        content.style.maxHeight = "80px";
+      }
+    });
 }
 
 console.log("js loaded");
